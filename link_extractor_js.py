38f98798b80f1308d91c74dc629993d6ddb9e03e
Major2000 <majormadobe@gmail.com>
2022-10-26 15:21:43 +0300

    update link_extractor_js.py initialize colorama

--- link_extractor_js.py
+++ link_extractor_js.py
@@ -2,3 +2,11 @@ from requests_html import HTMLSession
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 import colorama
+
+
+colorama.init()
+
+GREEN = colorama.Fore.GREEN
+GRAY = colorama.Fore.LIGHTBLACK_EX
+RESET = colorama.Fore.RESET
+YELLOW = colorama.Fore.YELLOW
